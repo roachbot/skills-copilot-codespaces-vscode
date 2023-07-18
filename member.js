8fd3b8b865a178678789b7107d307b22bf98d15e
Roachbot <thecrazyroach@hotmail.com>
2023-07-18 23:32:45 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember()
+{
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var projects = document.getElementById("projects");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
+    var memberInfo = document.getElementById("memberInfo");
+    var skillsInfo = document.getElementById("skillsInfo");
+    var projectsInfo = document.getElementById("projectsInfo");
+    var contactInfo = document.getElementById("contactInfo");
+}
+
